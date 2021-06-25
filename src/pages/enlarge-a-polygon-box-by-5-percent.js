718a0a51561a41b20da227be69e26a4b09611a3c
Grzegorz Tomicki <gtomik@gmail.com>
2021-06-25 09:28:10 +0200

    feat: new examples

---
+++ src/pages/enlarge-a-polygon-box-by-5-percent.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react';
+import { MapContainer, useMap, TileLayer, Polyline } from 'react-leaflet';
+import { polygon, transformScale } from '@turf/turf';
+
+const center = [52.22977, 19.01178];
+
+const poland = [
+  [54.80068486732236, 18.292236328125004],
+  [53.89786522246521, 14.611816406250002],
+  [51.055207338584964, 15.281982421875002],
+  [49.57510247172322, 19.138183593750004],
+  [50.57626025689928, 23.642578125000004],
+  [52.214338608258224, 23.148193359375004],
+  [52.86912972768522, 23.741455078125],
+  [54.29729354239267, 22.928466796875004],
+  [54.29729354239267, 19.489746093750004],
+  [54.80068486732236, 18.292236328125004]
+];
+
+const MyCountry = () => {
+  const map = useMap();
+  const [newCoordinates, setCoordinates] = useState(null)
+
+  useEffect(() => {
+    if (!map) return
+
+    const poly = polygon([poland]);
+    const scalePoly = transformScale(poly, 1.05)
+
+    const { coordinates } = scalePoly.geometry;
+
+    console.log(coordinates);
+    setCoordinates(coordinates);
+
+  }, [map])
+
+  return newCoordinates
+    ? (
+      <>
+        <Polyline positions={poland} fill={'red'} color={'red'} />
+        <Polyline positions={newCoordinates} fill={'white'} color={'white'} />
+      </>
+    )
+    : null
+
+}
+
+const MapWrapper = () => {
+  return (
+    <MapContainer
+      center={center} zoom={6}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      <MyCountry />
+
+    </MapContainer>
+  )
+}
+
+export default MapWrapper;
---
+++ src/pages/part-of-the-common.js
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react';
+import { useMap, MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import { circle, intersect } from '@turf/turf';
+
+const center = [52.22977, 21.01178];
+
+const centers = [
+  { lat: 52.22990558765487, lng: 21.01168513298035 },
+  { lat: 52.22962958994604, lng: 21.011593937873844 },
+  { lat: 52.2297445891999, lng: 21.012012362480167 }
+]
+
+const options = {
+  steps: 64,
+  units: 'meters',
+  options: {}
+}
+
+const intersectionColor = {
+  color: "yellow",
+  weight: 2,
+  opacity: 1,
+  fillColor: "yellow",
+  fillOpacity: 0.7
+};
+
+const radius = 30;
+
+const Intersection = () => {
+  const map = useMap();
+  const [polygons, setPolygons] = useState([])
+
+  useEffect(() => {
+
+    centers.map(({ lat, lng }) => {
+      const polygon = circle([lng, lat], radius, options);
+
+      return setPolygons(pol => [...pol, polygon]);
+    })
+
+  }, [map])
+
+  return polygons.length > 0
+    ? (
+      <>
+        <GeoJSON data={polygons} color={'red'} weight={2} />
+        <GeoJSON data={intersect(...polygons)} style={intersectionColor} />
+      </>
+    )
+    : null
+}
+
+
+const MapWrapper = () => {
+  return (
+    <MapContainer center={center} zoom={18} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      <Intersection />
+
+    </MapContainer>
+  )
+}
+
+export default MapWrapper;
